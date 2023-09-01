96a30cb22776cec7784521b33df7eb2bc54051ed
bllyjulian/billy-app <billyjulian75@gmail.com>
2023-09-01 10:50:36 +0700

    imam

---
+++ app/src/main/java/com/y/coba/MainActivity.java
@@ -0,0 +1,17 @@
+package com.y.coba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView txtName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        txtName.findViewById(R.id.txt_name);
+//        txtName.setText("Imam Coba");
+    }
+}
